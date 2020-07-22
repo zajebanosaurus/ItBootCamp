d073c0abd8d60def63948590ee67db73ce96275c
Slobodan <mr.slobodan@hotmail.rs>
2020-07-22 16:51:33 +0200

    dodat zadatak 3 i 4

--- 2.Nedelja/Sreda/zadatak1.js
+++ 2.Nedelja/Sreda/zadatak1.js
@@ -11,8 +11,10 @@ let b = 4
         console.log("Proizvod je: " + proizvod)
     let kolicnik = a / b
         console.log("Količnik je: " + kolicnik)
-    let ostatak = a % b;
+    let ostatak = a % b
         console.log("Ostatak brojeva je: " + ostatak)
+    let stepen = Math.pow(a,b)
+        console.log("Stepen brojeva je: " + stepen)
 }    
 // Skraćena varijant
 {
@@ -20,7 +22,8 @@ let b = 4
     let razlika = a - b
     let proizvod = a * b
     let kolicnik = a / b
-    let ostatak = a % b;
+    let ostatak = a % b
+    let stepen = Math.pow(a,b)
 
-    console.log(zbir, razlika, proizvod, kolicnik, ostatak)
+    console.log(`zbir brojeva je: ${zbir}, razlika brojeva je: ${razlika}, proizvod brojeva je: ${proizvod}, količnik brojeva je: ${kolicnik}, ostatak brojeva je: ${ostatak}, stepen brojeva je: ${stepen}`)
 }
---
+++ 2.Nedelja/Sreda/zadatak3.js
@@ -0,0 +1,8 @@
+let broj = 11
+{
+    if(broj % 3 === 0){
+        console.log(`broj: ${broj} je deljiv sa 3`)
+    }else {
+        console.log(`broj: ${broj} nije deljiv sa 3`)  
+    }
+}
