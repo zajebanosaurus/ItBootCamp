9af3d9d3e1b4110594224edf9fbe1df207bb0178
Slobodan <mr.slobodan@hotmail.rs>
2020-07-29 13:13:54 +0200

    3.Nedelja/Sreda prvi zadatak

---
+++ 3.Nedelja/Sreda/zadatak1.js
@@ -0,0 +1,20 @@
+// Написати функцију која враћа максимум од 3 броја (Параметри функције су 3 броја)
+
+let n1 = 76
+let n2 = 10
+let n3 = -34
+let max = 0
+
+function maxOfNumbers(n1,n2,n3){
+    if(n1 > n2 && n1 > n3){
+        max = n1
+    }else if(n2 > n1 && n2 > n3){
+        max = n2
+    }else{
+        max = n3
+    }
+    return max
+}
+maxOfNumbers(n1,n2,n3)
+
+console.log(`Maksimum od 3 broja je: ${max}`)
