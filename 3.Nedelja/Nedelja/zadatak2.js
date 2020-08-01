f73041b67aea9a1c165a8c2a7c7a532135b00762
Slobodan <mr.slobodan@hotmail.rs>
2020-08-01 15:18:30 +0200

    promena zadatak2

--- 3.Nedelja/Nedelja/zadatak2.js
+++ 3.Nedelja/Nedelja/zadatak2.js
@@ -1,13 +1,11 @@
 // Написати функцију која враћа колико пице вам је довољно за доживотно снабдевање
 // Уноси се колико пице поједете на месечном нивоу, и колико година имате (Рачуна се да је довољно достављати до стоте године)
 
-function lifeSupply(numPerMonth,age){
-    let years = 0
-    for(let i = age; i <= 100; i++){
-        years = i - age
-    }
-    return years * (numPerMonth * 12)
-}
 
-lifeSupply(10,26) // Враћа 8880
+const lifeSupply = (numPerMonth,age) => (100 - age) * (numPerMonth * 12)
+
+
+lifeSupply(10,99) // Враћа 8880
 console.log(lifeSupply(10,26)) // Исписује 8880
+
+
