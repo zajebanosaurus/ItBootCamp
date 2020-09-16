1354cb285dae796e0789bdf61dd870134181f68f
Slobodan <mr.slobodan@hotmail.rs>
2020-09-16 15:12:10 +0200

    ispravka

--- 9.Nedelja/sreda/src/components/User.jsx
+++ 9.Nedelja/sreda/src/components/User.jsx
@@ -1,10 +1,21 @@
-import React, { useState } from 'react'
+import React, { useState }  from 'react'
 
 const User = ({user, searchUser, users, setUsers }) => {
     const [id, setId] = useState('')
 
     const handleClick = () => {
-        setUsers([...users].splice(users.findIndex(el => el.key === id),1))
+
+        // OVO RADI
+        let tmp = [...users]
+        tmp.splice(tmp.findIndex(el => el.id === id),1)
+        setUsers(tmp)
+
+        // OVO JOK! ALI ZAŠTO?
+        // setUsers([...users].splice(users.findIndex(el => el.id === id),1))
+
+        // I OVO
+        // let tmp = [...users].splice(users.findIndex(el => el.id === id),1)
+        // setUsers(tmp)
     }
     return (
         <div className="users" onMouseEnter={() => setId(user.id)} onMouseLeave={() => setId('')}>
